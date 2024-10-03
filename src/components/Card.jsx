ec0f00ff2398b95e040f1321fae23e158f245b96
Guillaume Lamarque <guillaumelamarque7@gmail.com>
2024-10-03 10:41:25 +0200

    Ajoute un spinner de chargement sur les cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 // Composant Logements qui reçoit les données des logements en tant que prop
@@ -5,10 +6,18 @@ const Card = ({ logementsData }) => {
   // Hook useNavigate pour la navigation programmatique
   const navigate = useNavigate();
 
+  // State pour suivre le chargement des images
+  const [loading, setLoading] = useState({});
+
   // Fonction pour gérer le clic sur un logement
   const handleClick = (id) => {
     // Navigue vers la page du logement avec l'ID correspondant
     navigate(`/logement/${id}`);
+  };
+
+  // Fonction pour gérer le chargement de l'image
+  const handleImageLoad = (id) => {
+    setLoading((prevLoading) => ({ ...prevLoading, [id]: false }));
   };
 
   return (
@@ -23,8 +32,17 @@ const Card = ({ logementsData }) => {
           onClick={() => handleClick(logement.id)} // Ajoute un gestionnaire de clic pour naviguer vers le logement
         >
           <div className='card-thumbnail_fade'></div>
+          {/* Affiche une animation de chargement tant que l'image n'est pas chargée */}
+          {loading[logement.id] !== false && (
+            <div className='card-thumbnail_spinner'><i className='fas fa-spinner'></i></div>
+          )}
           {/* Image de couverture du logement */}
-          <img src={logement.cover} alt={logement.title} />
+          <img
+            src={logement.cover}
+            alt={logement.title}
+            onLoad={() => handleImageLoad(logement.id)}
+            style={{ display: loading[logement.id] === false ? 'block' : 'none' }}
+          />
           {/* Titre du logement */}
           <h2>{logement.title}</h2>
         </div>
@@ -33,5 +51,4 @@ const Card = ({ logementsData }) => {
   );
 };
 
-// Exporte le composant Logements pour l'utiliser dans d'autres parties de l'application
 export default Card;
